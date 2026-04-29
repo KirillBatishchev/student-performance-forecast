c18640b9530d8f0b0cfea47ef85e6703d1b73de8
KirillBatishchev <batishchevkirill@gmail.com>
2026-04-30 00:12:31 +0300

    chore: initialize project structure with cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predicting the probability of successful completion of the next block of tasks by a student based oт the EdNet3 dataset using transformers.',
+    author='Batishchev Kirill & Lopin Ivan',
+    license='MIT',
+)
